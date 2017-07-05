54b02021fadc2da6d089055f9dbb1307d23235b0
TomoyaShibata <wind.of.hometown@gmail.com>
2017-07-05 21:55:27 +0900

    :+1: Add `learnjs.showView` method

--- public/app.js
+++ public/app.js
@@ -1,2 +1,7 @@
 'use strict'
 const learnjs = {}
+
+learnjs.showView = (hash) => {
+  const problemView = $('<div class="problem-view">').text('Coming soon!')
+  $('.view-container').empty().append(problemView)
+}
